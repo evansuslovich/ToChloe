8dfb9216b570469b7c4b73506d7473726f67a4e1
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-25 01:11:49 -0400

    letterApi

--- client/src/app/services/api/letterApi.js
+++ client/src/app/services/api/letterApi.js
@@ -3,29 +3,33 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const lettersApi = createApi({
   reducerPath: 'lettersApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3001/letters/'
+    baseUrl: 'http://localhost:3001/api/letters/',
+    prepareHeaders: (headers,) => {
+      headers.set('x-access-token', localStorage.getItem('token'));
+      return headers
+    },
   }),
 
   endpoints: (builder) => ({
 
     getLetters: builder.query({
       query: () => ({
-        url: 'createLetter',
+        url: 'getLetters',
         method: 'GET',
       }),
     }),
 
     createLetter: builder.mutation({
-      query: (collection) => ({
+      query: (letter) => ({
         url: 'createLetter',
         method: 'POST',
-        body: { ...collection },
+        body: { ...letter },
       }),
     }),
   }),
 });
 
 export const {
-  useGetLettersMutation,
+  useGetLettersQuery,
   useCreateLetterMutation
 } = lettersApi;
