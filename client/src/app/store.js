6e555c4b0f2d3f061ca03793a065694af77b1dc4
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-25 01:11:03 -0400

    store created, added lettersApi.reducerPath to reducer and middleware, setUpListener (recommended by RTK documentation)

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,24 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import { lettersApi } from './services/api/letterApi';
+import lettersReducer from './services/slices/letterSlice'
+
 import authReducer from './services/slices/authSlice'
-import { authApi } from './services/api/authApi'
+import { authApi } from './services/api/authApi';
+
 
 let store; // singleton store 
 
 const setupStore = () => {
-  
-  return configureStore({
+
+  const theStore = configureStore({
     reducer: {
       [authApi.reducerPath]: authApi.reducer,
       auth: authReducer,
+
+      [lettersApi.reducerPath]: lettersApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
       authApi.middleware,
+      lettersApi.middleware,
     ),
-  })
+  });
 
-}
+  // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+  // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+  setupListeners(theStore.dispatch)
+
+  return theStore
+
+};
+
 
 // acts as a singleton factory method
 const getStore = () => {
