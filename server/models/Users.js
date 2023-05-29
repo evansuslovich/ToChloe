1400afc2aa19e17af4aceb533915ac232cbe8bd1
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-28 22:58:38 -0400

    Users model, userImage, list for requests, list for friends

--- server/models/Users.js
+++ server/models/Users.js
@@ -19,8 +19,23 @@ module.exports = (sequelize, DataTypes) => {
     },
     token: {
       type: DataTypes.STRING,
-      allowNull: true 
-    }
+      allowNull: true
+    },
+    userImage: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }, sentRequest: [{
+      username: { type: String, default: '' }
+    }],
+    request: [{
+      userId: { type: DataTypes.Schema.Types.ObjectId, ref: 'User' },
+      username: { type: String, default: '' }
+    }],
+    friendsList: [{
+      friendId: { type: DataTypes.Schema.Types.ObjectId, ref: 'User' },
+      friendName: { type: String, default: '' }
+    }],
+    totalRequest: { type: Number, default: 0 }
   });
 
   return Users;
