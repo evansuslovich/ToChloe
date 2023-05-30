dab4f9373491dd2c907bdde196d454aa37902904
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-30 11:08:59 -0400

    log err

--- server/routes/Letters.js
+++ server/routes/Letters.js
@@ -8,22 +8,17 @@ const auth = require("../../server/middleware/auth")
 const { Letters } = require("../models")
 
 router.post("/createLetter", async (req, res) => {
-
   try {
 
     // get letter fromr request
     const { letter } = req.body
 
     // create letter
-    const createdLetter = await Letters.create({
-      letter: letter
-    })
-
+    const createdLetter = await Letters.create({ letter: letter })
     return res.status(200).json(createdLetter)
   } catch (err) {
+    console.log(err)
   }
-
-
 })
 
 module.exports = router 
