11bb6b032974cc470f905a9f8030146579596481
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-09 00:59:34 -0400

    parameters are empty validation and user can't send a message to themselves

--- server/routes/Letters.js
+++ server/routes/Letters.js
@@ -10,9 +10,19 @@ router.post("/createLetter", async (req, res) => {
   // get letter, fromUserId, and toUserId from the request
   const { letter, fromUserId, toUserId } = req.body
 
+  console.log(req.body)
+
 
   let fromUser;
   let toUser;
+
+  if (!letter || !fromUserId || !toUserId) {
+    return res.status(403).json({ "message": "Parameters are empty" })
+  }
+
+  if (fromUserId === toUserId) {
+    return res.status(403).json({ "message": "You can't send a message to yourself" })
+  }
 
   // if the length of the letter is 0 (empty) 
   if (letter.length === 0) {
@@ -22,14 +32,14 @@ router.post("/createLetter", async (req, res) => {
   // get the user sending the letter
   fromUser = await Users.findOne({ where: ({ id: fromUserId }) })
   // if the user sendind the letter does not exist, throw an error
-  if (fromUser === null || fromUser === undefined) {
+  if (fromUser === null) {
     return res.status(403).json({ "message": "The user sending this letter does not exist" })
   }
 
   // get the user sending the letter
   toUser = await Users.findOne({ where: ({ id: toUserId }) })
   // if the user receiving the letter does not exist, throw an error
-  if (toUser === null || toUser === undefined) {
+  if (toUser === null) {
     return res.status(403).json({ "message": "The user receiving this letter does not exist" })
   }
 
