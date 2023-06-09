f3677ad0d7efac0e3c788621455a6e1cf80ecab8
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-09 01:00:02 -0400

    reverted to an older versio of WriteLetter, need to test the API

--- client/src/features/letters/WriteLetter.jsx
+++ client/src/features/letters/WriteLetter.jsx
@@ -1,89 +1,68 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import { useCreateLetterMutation } from "../../app/services/api/letterApi";
-import { useSnackbar } from 'notistack';
+import { useSnackbar } from 'notistack'
 import Button from '@mui/material/Button';
-import clickSound from './soundEffects/click.mp3';
+import TextField from '@mui/material/TextField';
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "../../app/services/slices/authSlice"
 
-import './_letterStyle.css';
-import { number } from "yup";
+
 
 export default function WriteLetter() {
-  const playSoundEffect = () => {
-    const audio = new Audio(clickSound);
-    audio.play();
-  };
 
-  const [letters, setLetters] = useState(['']);
-  const [numberOfCharacters, setNumberOfCharacters] = useState([0])
-  const { enqueueSnackbar } = useSnackbar();
-  const [createLetter, { isLoading }] = useCreateLetterMutation();
+  const user = useSelector(selectCurrentUser)
+  const [letterCredentials, setLetterCredentials] = React.useState({
+    letter: '', fromUserId: user.id, toUserId: ''
+  }); const { enqueueSnackbar } = useSnackbar()
+  const [createLetter, { isLoading }] = useCreateLetterMutation()
 
-  const handleChange = (event, index) => {
+  const handleChange = (event) => {
+    const name = event.target.name;
     const value = event.target.value;
-    playSoundEffect();
-
-    if (numberOfCharacters[index] >= 1500) {
-      const newLettersList = [...letters]
-      newLettersList.push([''])
-      setLetters(newLettersList);
-
-      const newCharacterList = [...numberOfCharacters];
-      newLettersList.push([0])
-      setNumberOfCharacters(newCharacterList)
-    } else {
-      const newLettersList = [...letters];
-      newLettersList[index] = value;
-      setLetters(newLettersList);
-
-      const newCharacterList = [...numberOfCharacters];
-      newCharacterList[index] = newLettersList[index].length
-      setNumberOfCharacters(newCharacterList)
-
-    }
-  };
-
-
-  const renderTextareas = () => {
-    return letters.map((letter, index) => (
-      <div>
-        <h1>{numberOfCharacters[index]}</h1>
-        <div className="paper" key={index}>
-          <textarea
-            className="paper-input"
-            value={letters[index]}
-            onChange={(e) => handleChange(e, index)}
-          />
-        </div>
-
-        <textarea
-          wrap="hard"
-          className="non-editable-text-area"
-          readOnly />
-      </div>
-    ))
+    setLetterCredentials(values => ({ ...values, [name]: value }))
   }
 
+  console.log(letterCredentials)
 
   return (
-    <div className="letter">
-      {renderTextareas()}
+    <div>
+      <TextField
+        margin="normal"
+        required
+        id="letter"
+        label="Letter"
+        name="letter"
+        autoComplete="Letter"
+        autoFocus
+        onChange={handleChange}
+      />
 
+      <TextField
+        margin="normal"
+        required
+        id="toUserId"
+        label="To User Id"
+        name="toUserId"
+        autoComplete="To User Id"
+        autoFocus
+        onChange={handleChange}
+      />
       <Button
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
         disabled={isLoading}
         onClick={async () => {
           try {
-            // const sentLetter = await createLetter(letter).unwrap();
+            const sentLetter = await createLetter(letterCredentials).unwrap();
             enqueueSnackbar('Letter Created', { variant: 'success' });
           } catch (err) {
-            console.log(err);
+            console.log(err)
             enqueueSnackbar('Letter Created Failed: ' + err, { variant: 'error' });
           }
         }}
       >
         Send Letter
       </Button>
-    </div>
-  );
+    </div >
+  )
 }
