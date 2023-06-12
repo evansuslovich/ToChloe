0d3331b74e320c8f217eda5261555ae29a3f144f
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-12 09:27:51 -0400

    added header to homepage

--- client/src/features/homepage/Homepage.jsx
+++ client/src/features/homepage/Homepage.jsx
@@ -1,10 +1,57 @@
-import React from "react"
-import Header from "../../header/Header"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { selectIsLoggedIn } from "../../app/services/slices/authSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../../app/services/api/authApi";
+import { setUser } from "../../app/services/slices/authSlice";
+import { useSnackbar } from 'notistack';
+import Intro from "./Intro";
+import WriteLetter from "../letters/WriteLetter";
+import Search from "../social/search/Search";
+import Header from "../header/Header";
 
-export default function Intro() {
+export default function Homepage() {
 
+  const [logout] = useLogoutMutation();
+  const auth = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
   return (
-    <Header />
 
+    <div>
+      <Header />
+      {
+        auth &&
+        <div>
+          <Link to="/account"> Account </Link>
+
+          <Search />
+
+          <br />
+
+          <Link onClick={async () => {
+            await logout().unwrap();
+            dispatch(setUser(null));
+            localStorage.setItem('token', null)
+            navigate('/');
+            enqueueSnackbar("You've been logged out", { variant: 'info' });
+          }}>
+            Logout
+          </Link>
+
+          <WriteLetter />
+
+        </div>
+      }
+
+      {
+        !auth &&
+        <div>
+          <Intro />
+        </div>
+      }
+
+    </div >
   )
 }
