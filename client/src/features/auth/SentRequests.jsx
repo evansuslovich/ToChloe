48d19601b9b309b0853f52fb1d88a03f3c7a445d
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-11 22:21:44 -0400

    comment out

---
+++ client/src/features/auth/SentRequests.jsx
@@ -0,0 +1,23 @@
+// import { useSelector } from "react-redux"
+// import { selectCurrentUser } from "../../app/services/slices/authSlice"
+// import { NavLink } from "react-router-dom"
+
+// export default function Account() {
+
+//   const user = useSelector(selectCurrentUser)
+
+//   return (
+
+//     <div>
+//       <div>
+//         <h1>Account</h1>
+//         <h1>First Name: {user.firstName}</h1>
+//         <h1>Last Name: {user.lastName}</h1>
+//         <h1>username: {user.username}</h1>
+//         <NavLink to="/account/friends">Friends: {user.friendsList.length}</NavLink>
+//       </div>
+//     </div>
+
+
+//   )
+// }
