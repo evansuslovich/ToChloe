a66dda3d7e6bda8ece8e6be6bd59b965f3c6a896
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-14 22:29:09 -0400

    required authentication tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {
-  Route, Routes
+  Route, Routes, useLocation, Navigate
 } from 'react-router-dom';
 
 import Homepage from "./features/homepage/Homepage"
@@ -12,22 +12,59 @@ import Account from "./features/auth/Account";
 import User from "./features/social/User";
 import Friends from "./features/auth/Friends";
 import ReceivedRequests from "./features/auth/ReceivedRequests";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "./app/services/slices/authSlice";
 
 function App() {
 
   return (
     <div className="app">
       <Routes>
+
         <Route index element={<Homepage />} />
-        <Route path="/:username" element={(<User />)} />
-        <Route path="/account/friends" element={(<Friends />)} />
-        <Route path="/account/acceptRequests" element={(<ReceivedRequests />)} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/account" element={<Account />} />
+
+
+        <Route path="/account" element={
+          <RequireAuth>
+            <Account />
+          </RequireAuth>
+        }
+        />
+
+        <Route path="/:username" element={
+          <RequireAuth>
+            <User />
+          </RequireAuth>}
+        />
+
+        <Route path="/account/friends" element={
+          <RequireAuth>
+            <Friends />
+          </RequireAuth>} />
+
+        <Route path="/account/acceptRequests" element={
+          <RequireAuth>
+            <ReceivedRequests />
+          </RequireAuth>
+        }
+        />
+
       </Routes>
     </div>
   );
 }
+
+function RequireAuth({ children }) {
+  const auth = useSelector(selectIsLoggedIn);
+  const location = useLocation();
+
+  if (!auth) {
+    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+  }
+  return children;
+}
+
 
 export default App;
