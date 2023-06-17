59a25ed07281117b05a7d94042f43f7d2558f1e9
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-17 16:26:26 -0400

    finalized .. for now

--- client/src/features/header/Header.jsx
+++ client/src/features/header/Header.jsx
@@ -1,22 +1,17 @@
 import { NavLink, useNavigate } from "react-router-dom"
 import PersonIcon from '@mui/icons-material/Person';
 import IconButton from "@mui/material/IconButton";
+import UserMenu from "./UserMenu";
 
 export default function Header() {
 
-  const navigate = useNavigate()
 
   return (
     <div className="header">
       <NavLink className="logo" to="/">ToChloe</NavLink>
 
       <div className="account-logo">
-        <IconButton
-          onClick={() => { navigate("/account") }}
-          color="contrast"
-          aria-label="user">
-          <PersonIcon />
-        </IconButton>
+        <UserMenu />
       </div>
 
     </div>
