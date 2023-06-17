42ec68cc5ad248360427da677bc42c3c948f7a93
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-17 16:26:13 -0400

    intro

--- client/src/features/homepage/Homepage.jsx
+++ client/src/features/homepage/Homepage.jsx
@@ -1,53 +1,11 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom"
-import { selectIsLoggedIn } from "../../app/services/slices/authSlice";
-import { useSelector, useDispatch } from "react-redux";
-import { useLogoutMutation } from "../../app/services/api/authApi";
-import { setUser } from "../../app/services/slices/authSlice";
-import { useSnackbar } from 'notistack';
-import Intro from "./Intro";
-import WriteLetter from "../letters/WriteLetter";
-import Search from "../social/search/Search";
 import Header from "../header/Header";
 
 export default function Homepage() {
 
-  const [logout] = useLogoutMutation();
-  const auth = useSelector(selectIsLoggedIn);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { enqueueSnackbar } = useSnackbar();
   return (
 
     <div>
       <Header />
-      {
-        auth &&
-        <div>
-          <Search />
-          <br />
-
-          <Link onClick={async () => {
-            await logout().unwrap();
-            dispatch(setUser(null));
-            localStorage.setItem('token', null)
-            navigate('/');
-            enqueueSnackbar("You've been logged out", { variant: 'info' });
-          }}>
-            Logout
-          </Link>
-
-          <WriteLetter />
-
-        </div>
-      }
-
-      {
-        !auth &&
-        <div>
-          <Intro />
-        </div>
-      }
 
     </div >
   )
