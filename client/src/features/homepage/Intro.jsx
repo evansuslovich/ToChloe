012ac23ca44bbf11cdd25e7c8f52e680c11c07a7
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-06-20 21:42:43 -0400

    created css grid and 4 funny political cartoon jokes for the purpose of the project

--- client/src/features/homepage/Intro.jsx
+++ client/src/features/homepage/Intro.jsx
@@ -1,17 +1,56 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import internet_surveillance from "../../img/internet_surveillance.jpeg"
+import helicopter_parents from "../../img/helicopter_parents.jpeg"
+import usps from "../../img/usps.jpeg"
+import email from "../../img/emails.jpeg"
 
 export default function Intro() {
 
   return (
-    <div>
-      <h1>Hello ... are you supposed to be here?</h1>
-      <h1>Currently, this is for letter sending, but one day, it can be for anyone!</h1>
-      <h1>If you're supposed to be here</h1>
+    <div className="intro">
+      <div className="grid">
+        <div className="box">
+          <h1>
+            Are you afraid of internet surveillance?
+          </h1>
+          <img
+            src={internet_surveillance}
+            alt="I'm sorry"
+          />
+        </div>
 
-      <Link to="/sign-in"> Sign In </Link>
-      <Link to="/sign-up"> Sign Up </Link>
+        <div className="box">
+          <h1>
+            Are you afraid of your helicoptering parents?
+          </h1>
+          <img
+            src={helicopter_parents}
+            alt="I'm even more sorry"
+          />
+        </div >
+
+        <div className="box">
+          <h1>
+            Is the cornerstone of America's economy failing you?
+          </h1>
+
+          <img
+            src={usps}
+            alt="I'm really really sorry"
+          />
+        </div>
+
+        <div className="box">
+          <h1>
+            Are you only using online mailing for work or talking to your grandparents?
+          </h1>
+
+          <img
+            className="email"
+            src={email}
+            alt="I'm really really sorry"
+          />
+        </div>
+      </div>
     </div>
-
   )
 }
